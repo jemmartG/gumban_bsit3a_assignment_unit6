0994e31d4b8b5ac2839e3922c9b2fbc0357f9524
jemmartG <jemmart.gumban@wvsu.edu.ph>
2024-11-20 20:03:07 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
 
-      title: 'Despi',
+      title: 'Gumban',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 222, 229, 247))
         ),
